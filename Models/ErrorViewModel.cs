e931154c8d76e30180833d1ed4b626eb995fdc7a
kadija.elbedad@gmail.com <kadija@gmail.com>
2024-01-17 12:17:23 +0100

    First deploy back

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EcommerceKadelb_Back.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
